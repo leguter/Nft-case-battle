e47194c2a1c03a31a177af1383fda7c17b52c5a5
legut <levcenkokola06@gmail.com>
2025-09-25 22:53:32 +0300

    loginTg

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,30 @@
 
 import css from "./Header.module.css";
 import Logo from "../Logo/Logo"
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 const Header = () => {
-     const navigate = useNavigate();
+       const botUsername = 'Sanyajjj_bot';
+  const telegramLoginUrl = `https://t.me/${botUsername}`;
+     // const navigate = useNavigate();
 
   // Функція, яка буде викликатись при кліку на кнопку
-  const handleLoginClick = () => {
-    // Перенаправляємо користувача на сторінку /login
-    navigate('/login');
-  };
+//   const handleLoginClick = () => {
+//     // Перенаправляємо користувача на сторінку /login
+//     navigate('/login');
+//   };
      return (
     <div className={css.container}>
  <Logo />
 <h1 className={css.name}>NFT CASE BATTLE </h1>
-<button type="button" className={css.btn} onClick={handleLoginClick}>Увійти<img className={css.icon} src="../../../public/tgicon.svg" width="16px" height="16px"></img></button>
+<a 
+        href={telegramLoginUrl} 
+        className={css.btnLink}
+        target="_blank" 
+        rel="noopener noreferrer"
+      >
+        <button type="button" className={css.btn} >Увійти<img className={css.icon} src="../../../public/tgicon.svg" width="16px" height="16px"></img></button>
+      </a>
+
 </div>
      )
 };
