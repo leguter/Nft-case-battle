926b57aa88f9d4536ac6a6166378c34942f3751b
legut <levcenkokola06@gmail.com>
2025-10-08 01:19:01 +0300

    11

--- src/components/CaseContent/CaseContent.jsx
+++ src/components/CaseContent/CaseContent.jsx
@@ -218,7 +218,7 @@ const CaseContent = ({ caseItem,user, setUser }) => {
       </span>
     </div>
                 {/* <span className={styles.stars}>{item.stars} ⭐</span> */}
-                <span className={styles.usd}>{item.tonPrice}<img className={styles.ton} src="/ton-symbol.svg" width="16px" height="16px"></img></span>
+                <span className={styles.usd}>{item.tonPrice}<img className={styles.ton} src="/public/ton-symbol.svg" width="16px" height="16px"></img></span>
               </div>
             </div>
           </div>
